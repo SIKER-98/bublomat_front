5f187ee8aada105c8b8e2c92da5011e6cd15fc80
Mati-7 <81324439+Mati-7@users.noreply.github.com>
2021-04-16 12:18:39 +0200

    Update ProductCard.js

--- src/components/productCard/ProductCard.js
+++ src/components/productCard/ProductCard.js
@@ -6,14 +6,11 @@ class ProductCard extends React.Component {
     render() {
         return (
             <div className={'searchedElement'}>
-                <img src={'img/wisnia.jpg'} alt={'img'}/>
+                <img src={this.props.img} alt={`img ${this.props.name}`}/>
                 <div className={'productInfo'}>
-                    <h1>Osobnik niereformowalny</h1>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                        popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                        and more recently with desktop publishing software like Aldus PageMaker including versions of
-                        Lorem Ipsum.</p>
-                    <h2>Rating: <span>66.6%</span></h2>
+                    <h1>{this.props.name}</h1>
+                    <p>{this.props.description}</p>
+                    <h2>Rating: <span>{this.props.rating}%</span></h2>
                 </div>
             </div>
         )
