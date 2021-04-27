31443cd0f442b52aecc3ce40e43d308d2f3acb34
SIKER <hubert.sikorski@yahoo.com>
2021-04-27 21:08:28 +0200

    zmiany do autentykacji

---
+++ src/components/error/ErrorComponent.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+function ErrorComponent() {
+    return <div>An error occurred. I don't know what to do! Contact support at dwaSzympansyI@zoo.com</div>
+}
+
+export default ErrorComponent;
